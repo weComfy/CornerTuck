462751a7279e2b1b3b4366dfa0235892935898ee
weComfy <149069531+weComfy@users.noreply.github.com>
2025-01-25 01:46:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const defaultFontSize = parseFloat(window.getComputedStyle(document.body).fontSize);
 let currentFontSize = defaultFontSize;
 
-const increaseTextBtn = document.getElementById("increase-text");
-const decreaseTextBtn = document.getElementById("decrease-text");
+const increaseTextBtn = document.getElementById(`increase-text`);
+const decreaseTextBtn = document.getElementById(`decrease-text`);
 
-increaseTextBtn.addEventListener("click", () => {
+increaseTextBtn.addEventListener(`click`, () => {
   currentFontSize += 2;
   document.body.style.fontSize = `${currentFontSize}px`;
 })
 
-decreaseTextBtn.addEventListener("click", () => {
+decreaseTextBtn.addEventListener(`click`, () => {
   currentFontSize -= 2;
   document.body.style.fontSize = `${currentFontSize}px`;
 })
@@ -18,7 +18,7 @@ const sidebar = document.getElementById('sidebar');
 const menuButton = document.querySelector('.menu-button');
 
 function toggleSidebar() {
-  sidebar.classList.toggle("open");
+  sidebar.classList.toggle(`open`);
   /*const currentWidth = sidebar.style.width;
   if (currentWidth === "250px") {
     sidebar.style.width = "0";
@@ -34,7 +34,7 @@ menuButton.addEventListener('click', function(event) {
 
 document.addEventListener('click', function(event) {
   if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
-    sidebar.classList.remove("open");
+    sidebar.classList.remove(`open`);
     //sidebar.style.width = '0';
  }
 })
